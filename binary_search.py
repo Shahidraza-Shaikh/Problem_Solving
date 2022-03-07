f79045bed005223ead8b05ec541374fe2f0f0dfa
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-03-07 23:59:14 +0530

    chnages in binary search

--- binary_search.py
+++ binary_search.py
@@ -12,7 +12,8 @@ def binary_search(arr,l,h,key):
 			return binary_search(arr,l,mid-1,key)
 
 		return binary_search(arr,mid+1,h,key)
+	return -1
 
 arr = [-5,0,4,5,8,9,10,56,89]
-key = -1
+key = 454
 print(binary_search(arr,0,len(arr)-1,key))
