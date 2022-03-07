0ebdc06086ee4ff4900312bd7155b55ca6d0a6c2
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-03-07 23:57:59 +0530

    binary search

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+
+
+def binary_search(arr,l,h,key):
+	if l <=h:
+		mid = (l+h)//2
+
+
+		if arr[mid] == key:
+
+			return mid
+		if arr[mid] > key :
+			return binary_search(arr,l,mid-1,key)
+
+		return binary_search(arr,mid+1,h,key)
+
+arr = [-5,0,4,5,8,9,10,56,89]
+key = -1
+print(binary_search(arr,0,len(arr)-1,key))
