ed6110d336407212e5c17ac3a1f2b7150e0baa5f
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-03-08 23:14:24 +0530

    output comment is added

--- find_combi_of_arr_size_k.py
+++ find_combi_of_arr_size_k.py
@@ -21,4 +21,16 @@ k = int(input())
 combinations(num,1,k)
 print(ans)
 
+# Find combinations of array
+# Output :-
 
+# if n = 4 and k=2
+# [1,2,3,4]  nCr i.e 4C2 == n!/k!(n-k)!
+
+# [[1,2],
+#  [1,3],
+#  [1,4],
+#  [2,3],
+#  [2,4],
+#  [3,4]
+# ]
