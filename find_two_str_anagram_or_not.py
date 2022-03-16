453b2c15f74c312989df5de67cc4c5138124eda6
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-03-16 23:51:16 +0530

    fint two string is anagram of eachother

---
+++ find_two_str_anagram_or_not.py
@@ -0,0 +1,24 @@
+
+def anagram(str1,str2):
+
+	n1 = len(str1)
+	n2 = len(str2)
+
+	if n1 != n2:
+		return 0
+
+	str1 = sorted(str1)
+	str2 = sorted(str2)
+
+	for s in range(n1):
+
+		if str1[s] != str2[s]:
+			return 0
+
+	return 1
+
+
+str1 = "silent"
+str2 = "listen"
+
+print(anagram(str1,str2))
