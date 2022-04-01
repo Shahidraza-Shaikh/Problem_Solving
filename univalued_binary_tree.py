8035f18eb86bba5e2feec8939bcbf5fe8469e23c
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-04-01 23:40:36 +0530

    checking weather Btree is univariate or no

---
+++ univalued_binary_tree.py
@@ -0,0 +1,33 @@
+
+class Node:
+
+	def __init__(self,data):
+
+		self.data = data
+		self.left = None
+		self.right = None
+
+
+root = Node(1)
+
+root.left = Node(1)
+
+root.right = Node(1)
+
+root.left.right = Node(4)
+root.left.left = Node(1)
+
+
+def univalued_Btree(root,val):
+
+	if root is None:
+		return True
+	if root.data != val:
+		return False
+	left = univalued_Btree(root.left,val)
+	right = univalued_Btree(root.right,val)
+
+	return left and right
+
+print(univalued_Btree(root,root.data))
+
