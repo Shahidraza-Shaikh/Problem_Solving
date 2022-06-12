629d90271b9de438a2aeda817a5faea52388f890
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-06-12 23:54:01 +0530

    ..

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -2,7 +2,8 @@ mat1 = [[1,2],
        [3,4]]
 
 mat2 = [[5,4],
-        [6,7]]
+        [6,7],
+        [1,2]]
 
 res = [[0,0],[0,0]]
 
@@ -12,8 +13,8 @@ res = [[0,0],[0,0]]
 for i in range(len(mat1)):
 
 	for j in range(len(mat1[0])):
-		print(mat1[i][j],mat2[i][j])
-		res[i][j] = mat1[i][j] * mat2[i][j]
+		# print(mat1[i][j],mat2[i][j])
+		# res[i][j] = mat1[i][j] * mat2[i][j]
 
 		for k in range(len(mat2)):
 			res[i][j] += mat1[i][k] * mat2[k][j]
