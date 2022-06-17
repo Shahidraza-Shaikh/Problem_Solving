4f098fa582d98e42dd59996d143a684d5c3d913d
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-06-17 23:47:10 +0530

    chnage value to zero if there is zero in matrix

---
+++ Set_Matrix_Zero.py
@@ -0,0 +1,51 @@
+
+
+
+matrix = [[1,1,1],[1,0,1],[1,1,1]]
+
+for i in range(len(matrix)):
+
+	for j in range(len(matrix[0])):
+
+		if matrix[i][j] == 0:
+
+			idx = i-1
+
+			while idx>=0:
+
+				if matrix[idx][j] != 0:
+
+					matrix[idx][j] = 0
+				idx -=1
+
+			idx = i +1
+
+			while idx < len(matrix) :
+
+				if matrix[idx][j] != 0 :
+					matrix[idx][j] = 0
+
+				idx +=1
+
+			idx = j-1
+
+			while idx >= j:
+
+				if matrix[i][idx] != 0:
+					matrix[i][idx] = 0
+
+				idx -=1
+
+			idx = j+1
+
+			while idx < len(matrix[0]):
+
+				if matrix[i][idx] != 0:
+
+					matrix[i][idx] = 0
+
+				j +=1
+
+print(matrix)
+
+
