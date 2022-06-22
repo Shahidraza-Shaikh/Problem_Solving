b9ec8cbbdd350a50838973eb286599f5289636bc
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-06-22 21:14:54 +0530

    aptimized code is added

--- recursion/FrogJump_To_Reach_Last_Stare.py
+++ recursion/FrogJump_To_Reach_Last_Stare.py
@@ -3,6 +3,7 @@
 
 #### Brute Force Approach
 
+'''
 def FrogJump(n,heights):
 
 	if n == 0:
@@ -17,11 +18,37 @@ def FrogJump(n,heights):
 		second_step = FrogJump(n-2,heights) + abs(heights[n]- heights[n-2])
 
 	return min(first_step,second_step)
+'''
+
+
+# Optimal approach using DP
+
+def FrogJump(n,heights,dp):
+
+	if n==0:
+		return 0
+
+	if dp[n] != -1:
+		return dp[n]
+
+	first_step = FrogJump(n-1,heights,dp) + abs(heights[n] - heights[n-1])
+
+	second_step = int(1e9) + 7
+
+	if n > 1 :
+
+		second_step = FrogJump(n-2,heights,dp) +  abs(heights[n] -  heights[n-2])
+
+	dp[n] = min(first_step,second_step)
+
+	return dp[n]
+
 
 heights =[7,5,5,8,4,9,1,1,10]
 
 n = len(heights)-1
 
-result = FrogJump(n,heights)
+dp = [-1] * (n+1)
+result = FrogJump(n,heights,dp)
 
 print(result)
