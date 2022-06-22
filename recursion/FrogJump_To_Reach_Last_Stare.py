e709ea328cab4b4ef8ed1cb3a4e655ef8329c70c
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-06-22 20:54:57 +0530

    find minimum energy to jump a frog from first to last stare

---
+++ recursion/FrogJump_To_Reach_Last_Stare.py
@@ -0,0 +1,27 @@
+
+#Question  https://www.codingninjas.com/codestudio/problem-details/frog-jump_3621012
+
+#### Brute Force Approach
+
+def FrogJump(n,heights):
+
+	if n == 0:
+		return 0
+
+	first_step = FrogJump(n-1,heights) + abs(heights[n] - heights[n-1])
+
+	second_step = int(1e9) + 7
+
+	if n > 1 :
+
+		second_step = FrogJump(n-2,heights) + abs(heights[n]- heights[n-2])
+
+	return min(first_step,second_step)
+
+heights =[7,5,5,8,4,9,1,1,10]
+
+n = len(heights)-1
+
+result = FrogJump(n,heights)
+
+print(result)
