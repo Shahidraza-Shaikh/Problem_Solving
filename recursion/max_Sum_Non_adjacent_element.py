ee9f038f6aac245be2ed52e305eadd6a5a2744de
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-06-23 23:45:34 +0530

    optimized code sol of the question

--- recursion/max_Sum_Non_adjacent_element.py
+++ recursion/max_Sum_Non_adjacent_element.py
@@ -4,6 +4,7 @@
 
 # Brute force for finding Max Sum of an Non Adjacent element
 
+'''
 def MaxSumNonAdj(nums,n):
 
 	if n == 0 :
@@ -18,10 +19,35 @@ def MaxSumNonAdj(nums,n):
 
 	return max(f1,f2)
 
+'''
+
+# Optimized solution
+
+def MaxSumNonAdj(nums,n,dp):
+
+	if n == 0 :
+		return nums[n]
+
+	if n  < 0 :
+		return 0
+	if dp[n] != -1 :
+		return dp[n]
+
+	f1 = nums[n] + MaxSumNonAdj(nums,n-2,dp)
+
+	f2 = 0 + MaxSumNonAdj(nums,n-1,dp)
+
+	dp[n] = max(f1,f2)
+
+	return dp[n]
+
 nums = [1,2,3,1,3,5,8,1,9]
+
 
 n = len(nums) - 1
 
-res = MaxSumNonAdj(nums,n)
+dp = [-1] * (n+1)
+
+res = MaxSumNonAdj(nums,n,dp)
 
 print(res)
