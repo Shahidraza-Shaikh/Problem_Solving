d905f256975d01ed0f02652cfddfcbb687ce926d
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-07-04 23:40:19 +0530

    find combination which has target sum

---
+++ recursion/combinationSum.py
@@ -0,0 +1,31 @@
+
+
+def combination(idx,arr,target,comb):
+
+	# if len(comb)
+
+	if idx == len(arr):
+
+		if target==0:
+
+			print(comb)
+			# return
+
+		# print("not pick")
+		return
+
+	if target>=arr[idx] :
+
+		comb.append(arr[idx])
+
+		combination(idx,arr,target-arr[idx],comb)
+
+		comb.pop()
+
+	combination(idx+1,arr,target,comb)
+
+
+arr = [5,1,7]
+target = 18
+comb = [ ]
+combination(0,arr,target,comb)
