07d5eda656ee23bdad9ce10e1d3d5d845696d286
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-07-22 23:48:19 +0530

    find leaf node and thier summision

---
+++ find_A_B_C_codechef.py
@@ -0,0 +1,18 @@
+n = 2 
+
+# f0 = 0
+# f1 = 3
+# f2 = 6
+func = [0,3,6]
+f = 0
+# for i in range(n+1):
+
+# 	for j in range(n+1):
+
+# 		f = i ^ j
+
+# 	if f == func[2]:
+# 		print(f)
+i,j,k,l = 0,0,0,0
+print(i,k)
+
