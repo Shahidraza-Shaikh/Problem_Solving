3a3677f09e2aec54fc8488124b976d52c0cabf06
Shahidraza-Shaikh <razashaikh8879@gmail.com>
2022-07-27 23:58:03 +0530

    check same value in binary tree'

--- Binary_Tree/univalued_BT.py
+++ Binary_Tree/univalued_BT.py
@@ -23,23 +23,12 @@ root.right.right = Node(0)
 def IsUnivalued(root,unival):
 
 	if root == None:
-
 		return True
-	print(root.data,unival)
-	if root.data != unival:
-		print("False")
 
+	if root.data != unival:
 		return False
 
-
-	# if root.left != None:
-	# if root
 	return IsUnivalued(root.left,unival) and IsUnivalued(root.right,unival)
-
-	# # if root.right != None :
-
-	# rightNode = 
-	# return leftNode and rightNode
 
 print(IsUnivalued(root,root.data))
 
